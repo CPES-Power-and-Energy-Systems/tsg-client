725107eba67ca138e66e7b12e05668335af8e941
jrsa <jose.r.andrade@inesctec.pt>
2024-01-04 02:45:07 +0000

    Removed helpers for now.

--- tsg_client/controllers/helpers.py
+++
@@ -1,9 +0,0 @@
-
-def parse_resources(ids_message):
-    catalogs = ids_message['ids:resourceCatalog']
-    data = []
-
-    for catalog in catalogs:
-        data.append({'catalog': catalog})
-
-    return
