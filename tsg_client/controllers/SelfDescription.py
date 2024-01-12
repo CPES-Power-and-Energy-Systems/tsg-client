2b82a82c6ca079ea8a8d335fe6951a33a361e4f5
Carolina Catorze <carolina.catorze@inesctec.pt>
2024-01-12 14:54:03 +0000

    feat: Add connector catalog parsing and artifact type-specific parsers  Closes #3and #5  This commit implements a parser for connector catalogs (using the 'selfdescription' class) and adds artifact type-specific parsers for PDF, JSON, CSV, etc.

---
+++ tsg_client/controllers/SelfDescription.py
@@ -0,0 +1,68 @@
+from typing import List, Dict
+from typing import Any
+from dataclasses import dataclass
+import json
+
+
+@dataclass
+class OfferedResource:
+    artifact_id: str
+    contract_offer: str
+    access_url: str
+
+    @staticmethod
+    def from_dict(obj: Any) -> 'OfferedResource':
+        _access_url = obj['ids:resourceEndpoint'][0]['ids:accessURL']['@id']
+        _path = obj['ids:resourceEndpoint'][0]['ids:path']
+        access_url = _access_url + _path
+        contract_offer = str(obj.get('ids:contractOffer', [''])[0])
+
+        if contract_offer != "":
+            artifact_id = obj['ids:representation'][0]['ids:instance'][0]['@id']
+        else:
+            artifact_id = obj['@id']
+
+        return OfferedResource(artifact_id, contract_offer, access_url)
+
+
+@dataclass
+class ResourceCatalog:
+    id: str
+    offeredResource: List[OfferedResource]
+
+    @staticmethod
+    def from_dict(obj: Any) -> 'ResourceCatalog':
+        _id = str(obj.get("@id"))
+        _offeredResource = [OfferedResource.from_dict(y) for y in obj.get("ids:offeredResource")]
+        return ResourceCatalog(_id, _offeredResource)
+
+
+@dataclass
+class SelfDescription:
+    id: str
+    title: str
+    description: str
+    securityProfile: str
+    curator: str
+    maintainer: str
+    endpoints: str
+    catalogs: List[ResourceCatalog]
+
+    @staticmethod
+    def from_dict(obj: Any) -> 'SelfDescription':
+        try:
+            _id = str(obj.get("@id"))
+            _title = str(obj.get("ids:title")[0].get("@value"))
+            _description = str(obj.get("ids:description")[0].get("@value"))
+            _security_profile = str(obj.get("ids:securityProfile").get("@id"))
+            _curator = str(obj.get("ids:curator").get("@id"))
+            _maintainer = str(obj.get("ids:maintainer").get("@id"))
+            _endpoints = str(obj.get("ids:hasDefaultEndpoint").get("ids:accessURL").get("@id"))
+            _catalogs = [ResourceCatalog.from_dict(y) for y in obj.get("ids:resourceCatalog")]
+            return SelfDescription(_id, _title, _description, _security_profile, _curator, _maintainer, _endpoints,
+                                   _catalogs)
+        except (KeyError, AttributeError, IndexError) as e:
+            raise ValueError(f"Error creating SelfDescription: {e}")
+
+    def json(self):
+        pass
---
+++ utils/file_handling.py
@@ -0,0 +1,26 @@
+import os
+import csv
+
+
+def save_text_file(artifact_id, content):
+    txt_filename = f"{artifact_id}.txt"
+    txt_path = os.path.join(os.getcwd(), txt_filename)
+    with open(txt_path, 'w', encoding='utf-8') as txt_file:
+        txt_file.write(content)
+    return {"message": f"Text file saved to {txt_path}"}
+
+
+def save_pdf_file(artifact_id, content):
+    pdf_filename = f"{artifact_id}.pdf"
+    pdf_path = os.path.join(os.getcwd(), pdf_filename)
+    with open(pdf_path, 'wb') as pdf_file:
+        pdf_file.write(content)
+    return {"message": f"PDF file saved to {pdf_path}"}
+
+
+def save_csv_file(artifact_id, content):
+    csv_filename = f"{artifact_id}.csv"
+    csv_path = os.path.join(os.getcwd(), csv_filename)
+    with open(csv_path, 'w', newline='', encoding='utf-8') as csv_file:
+        csv_file.write(content)
+    return {"message": f"CSV file saved to {csv_path}"}
